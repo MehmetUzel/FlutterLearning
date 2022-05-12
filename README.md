9adf5974ddbafd5b5c3828cb16a1944e9e637ff5
Mehmet Uzel <98mehmetuzel@gmail.com>
2022-05-12 14:34:00 +0300

    Project Initialization
    Also Local environment changed and made ready for flutter development

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FlutterLearning
 Learning Flutter
+
+https://www.youtube.com/watch?v=CD1Y2DmL5JM
+
+This tutorial used
+
---
+++ flutter_learn/android/app/src/main/kotlin/com/example/learning/flutter_learn/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.learning.flutter_learn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
