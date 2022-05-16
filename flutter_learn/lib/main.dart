409a52170924b6a8228c05a3e1d048e4c28e6a45
Mehmet Uzel <98mehmetuzel@gmail.com>
2022-05-16 15:15:02 +0300

    Page Navigation

--- flutter_learn/lib/main.dart
+++ flutter_learn/lib/main.dart
@@ -12,95 +12,141 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Remove Debug on Banner
+      home: firstPage(),
+    );
+  }
+}
+
+class firstPage extends StatefulWidget {
+  firstPage({Key? key}) : super(key: key);
+
+  @override
+  State<firstPage> createState() => _firstPageState();
+}
+
+class _firstPageState extends State<firstPage> {
   int num = 0;
   int indexnum = 0;
   Color maincolor = Colors.green.shade400;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, // Remove Debug on Banner
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Mehmet Çoban'),
-          backgroundColor: maincolor,
-        ),
-        body: Center(
-          child: indexnum == 0
-              ? Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  color: Colors.green,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          onPrimary: Colors.black,
-                          primary: Color.fromARGB(255, 104, 216, 108),
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            num++;
-                          });
-                        },
-                        child: Text('Hey + Click Me ' + num.toString()),
-                      ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          onPrimary: Colors.black,
-                          primary: Color.fromARGB(255, 104, 216, 108),
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            num--;
-                          });
-                        },
-                        child: Text('Hey - Click Me ' + num.toString()),
-                      ),
-                    ],
-                  ),
-                )
-              : Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  color: Colors.orange,
-                  child: Container(
-                    width: 200,
-                    height: 400,
-                    child: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mehmet Çoban'),
+        backgroundColor: maincolor,
+      ),
+      body: Center(
+        child: indexnum == 0
+            ? Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.green,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Image.asset('images/IMG_0288.jpeg'),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            onPrimary: Colors.black,
+                            primary: Color.fromARGB(255, 104, 216, 108),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              num++;
+                            });
+                          },
+                          child: Text('Hey + Click Me ' + num.toString()),
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            onPrimary: Colors.black,
+                            primary: Color.fromARGB(255, 104, 216, 108),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              num--;
+                            });
+                          },
+                          child: Text('Hey - Click Me ' + num.toString()),
+                        ),
                       ],
                     ),
-                  )),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: maincolor,
-          selectedItemColor: Colors.white,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.account_box),
-              label: 'No',
-            )
-          ],
-          currentIndex: indexnum,
-          onTap: (int index) {
-            setState(() {
-              if (indexnum == 0) {
-                indexnum = 1;
-                maincolor = Colors.orange.shade400;
-              } else {
-                indexnum = 0;
-                maincolor = Colors.green.shade400;
-              }
-            });
-          },
-        ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        onPrimary: Colors.black,
+                        primary: Color.fromARGB(255, 104, 216, 108),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(builder: (BuildContext context) {
+                            return const SecondPage();
+                          }),
+                        );
+                      },
+                      child: const Text('Next Page'),
+                    ),
+                  ],
+                ),
+              )
+            : Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.orange,
+                child: SizedBox(
+                  width: 200,
+                  height: 400,
+                  child: Column(
+                    children: [
+                      Image.asset('images/IMG_0288.jpeg'),
+                    ],
+                  ),
+                )),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: maincolor,
+        selectedItemColor: Colors.white,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box),
+            label: 'No',
+          )
+        ],
+        currentIndex: indexnum,
+        onTap: (int index) {
+          setState(() {
+            if (indexnum == 0) {
+              indexnum = 1;
+              maincolor = Colors.orange.shade400;
+            } else {
+              indexnum = 0;
+              maincolor = Colors.green.shade400;
+            }
+          });
+        },
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
       ),
     );
   }
